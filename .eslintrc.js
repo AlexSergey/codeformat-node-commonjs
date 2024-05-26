408cc392d296550dd495f88b05cba9e04285dd46
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2024-05-26 09:34:57 +0300

    feat: eslint 9 preparaion for use

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,7 +65,7 @@ module.exports = {
         'plugin:prettier/recommended',
         'plugin:perfectionist/recommended-natural',
         'plugin:regexp/recommended',
-        'plugin:sonarjs/recommended',
+        'plugin:sonarjs/recommended-legacy',
       ],
       rules: {
         'no-unused-vars': 'off',
