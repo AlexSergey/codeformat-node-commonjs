0f308344e5fe97991ffefff1e6d65de97d919ba2
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2025-05-30 14:46:20 +0200

    feat: replace sort-package-json to eslint plugin

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   '*.md': ['prettier --write'],
-  'package.json': ['npm run format:package'],
-  'src/**/*.ts': [() => 'npm run lint:ts'],
-  'src/**/*.{ts,json}': ['prettier --write', 'eslint --fix'],
+  '*.ts': [() => 'npm run lint:ts'],
+  '*.{ts,json}': ['prettier --write', 'eslint --fix'],
 };
